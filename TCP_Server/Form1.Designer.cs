538a0b58bde2fe126d5f474e31fa9cca348be66c
Adam Toda <adam368@poczta.onet.pl>
2017-12-21 19:41:55 +0100

    Fixed some previous errors

--- TCP_Server/Form1.Designer.cs
+++ TCP_Server/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comment_box = new System.Windows.Forms.TextBox();
+            this.port_box = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.ip_box = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.port_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comment_box
+            // 
+            this.comment_box.Location = new System.Drawing.Point(25, 88);
+            this.comment_box.Name = "comment_box";
+            this.comment_box.Size = new System.Drawing.Size(243, 22);
+            this.comment_box.TabIndex = 0;
+            // 
+            // port_box
+            // 
+            this.port_box.Location = new System.Drawing.Point(335, 23);
+            this.port_box.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port_box.Name = "port_box";
+            this.port_box.Size = new System.Drawing.Size(95, 22);
+            this.port_box.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(562, 116);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(463, 23);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(99, 23);
+            this.start_button.TabIndex = 5;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_ClickAsync);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Location = new System.Drawing.Point(463, 64);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(99, 23);
+            this.stop_button.TabIndex = 6;
+            this.stop_button.Text = "Stop";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // ip_box
+            // 
+            this.ip_box.Location = new System.Drawing.Point(42, 23);
+            this.ip_box.Name = "ip_box";
+            this.ip_box.Size = new System.Drawing.Size(243, 22);
+            this.ip_box.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 253);
+            this.Controls.Add(this.ip_box);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.port_box);
+            this.Controls.Add(this.comment_box);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.port_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox comment_box;
+        private System.Windows.Forms.NumericUpDown port_box;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.TextBox ip_box;
     }
 }
 
--- TCP_Server/Form1.cs
+++ TCP_Server/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace TCP_Server
 {
@@ -15,6 +17,68 @@ namespace TCP_Server
         public Form1()
         {
             InitializeComponent();
+        }
+        private TcpListener serwer;
+        Socket newSocket;
+        private async void start_button_ClickAsync(object sender, EventArgs e)
+        {
+            IPAddress adresIP;
+            
+            try
+            {
+                adresIP = IPAddress.Parse(ip_box.Text);
+            }
+            catch
+            {
+                ip_box.Text = String.Empty;
+                return;
+            }
+
+            int port = Convert.ToInt16(port_box.Value);
+            
+            try
+            {
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+                listBox1.Items.Add("Nasłuchiwanie rozpoczęte...");
+                listBox1.Update();
+
+                start_button.Enabled = false;
+                stop_button.Enabled = true;
+
+                newSocket = await serwer.AcceptSocketAsync();  //akceptacja klienta na kanale komunikacyjnym
+                if (newSocket.Connected)
+                {
+                    NetworkStream ns = new NetworkStream(newSocket);
+                    byte[] buf = Encoding.ASCII.GetBytes(comment_box.Text); // string zamieniamy na kod ASCII
+                    ns.Write(buf, 0, buf.Length);
+                    ns.Flush();  //wyzwolenie transmisji
+                    ns.Close();
+                }
+                listBox1.Items.Add("Komunikat został wysłany!");
+                listBox1.Update();
+                
+
+            }
+            catch
+            {
+                listBox1.Items.Add("błąd transmisji..");
+                listBox1.Update();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            newSocket.Close(); //zrywamy połączenie z klientem
+            serwer.Stop();
+            start_button.Enabled = true;
+            stop_button.Enabled = false;
+            comment_box.Text = String.Empty;
         }
     }
 }
